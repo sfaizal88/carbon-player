46a1fa62de93198673cc8d654376904aca543f9c
Ahamed Faizal <faizalahd4@gmail.com>
2024-05-27 11:09:10 +0800

    feat: Updating timing

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,8 +10,9 @@
 export const convertSecIntoMinSec = (seconds: number) => {
     // FINDING REMINDER SECOND AFTER TAKING OUT MINUTES
     const reminder = seconds % 60;
+    const availableMin = (seconds - reminder) / 60;
     // FINDING TOTAL MINUTES
-    const min = reminder <  60 ? 0 : (seconds - reminder) / 60;
+    const min = availableMin === 0 ? 0 : availableMin;
     // FINDING REMAINING SEC AFTER MIN TAKEN
     const sec  = reminder < 10 ? `0${reminder}` : reminder;
     return `${min < 10 ? '0'+min : min}:${sec}`;
