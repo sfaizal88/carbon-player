d389f9688ab6253bfc78f8332996c3def84d6b99
Ahamed Faizal <faizalahd4@gmail.com>
2024-05-27 10:46:01 +0800

    feat: Updating the min

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,5 +5,5 @@
  * @date - 25th May, 2024
  * 
  */
-export const DEFAULT_WORK_TIME = 10;
-export const DEFAULT_BREAK_TIME = 5;
+export const DEFAULT_WORK_TIME = 1500; // 25 min -> 1500 seconds
+export const DEFAULT_BREAK_TIME = 300; // 5 min -> 300 seconds
